c3be7d1e58c72bfd13e373bb8051b6499271cb27
Dmitry Davydenko <dimasik1204dim@gmail.com>
2022-10-22 03:46:44 +0300

    adaptive header upd

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,10 @@
-$(function () {
+const da = new DynamicAdapt("max");
+da.init();
 
+$(function () {
     $('.icon-menu').on('click', function () {
-        $('body').toggleClass('lock');
+        $('body').toggleClass('_lock');
         $('.icon-menu').toggleClass('menu-open');
         $('.menu__body').toggleClass('menu-open');
     })
-
 })
