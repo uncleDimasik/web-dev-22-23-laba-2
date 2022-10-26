8d3b6d730b966c764f240f02dbc74ed8eae752eb
Dmitry Davydenko <dimasik1204dim@gmail.com>
2022-10-26 07:35:34 +0300

    spoilers

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,9 @@
 const da = new DynamicAdapt("max");
 da.init();
 
+
 $(function () {
+
     $('.icon-menu').on('click', function () {
         $('.wrapper').toggleClass('_lock');
         $('.icon-menu').toggleClass('menu-open');
