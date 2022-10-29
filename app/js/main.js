4aa20f068104ad8bee496b08154290701afdc58d
Dmitry Davydenko <dimasik1204dim@gmail.com>
2022-10-29 06:03:45 +0300

    reviews

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,20 @@
 const da = new DynamicAdapt("max");
 da.init();
 
-
 $(function () {
 
     $('.icon-menu').on('click', function () {
         $('.wrapper').toggleClass('_lock');
         $('.icon-menu').toggleClass('menu-open');
         $('.menu__body').toggleClass('menu-open');
+        $('.review-slider').toggle('review-slider');
     })
+    $('.review-slider').slick({
+        dots: true,
+        infinite: true,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows:false,
+    });
 })
--- gulpfile.js
+++ gulpfile.js
@@ -43,6 +43,7 @@ function images() {
 function scripts() {
     return src([
         'node_modules/jquery/dist/jquery.js',
+        'node_modules/slick-carousel/slick/slick.min.js',
         'app/js/main.js'
     ])
         .pipe(concat('main.min.js'))
@@ -58,6 +59,7 @@ function include() {
         .pipe(dest('app'))
         .pipe(browserSync.stream())
 }
+
 
 function styles() {
     return src('app/scss/style.scss')
